458bdf3f5038155c53997b398db6eee91585fcec
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-30 09:39:53 -0700

    import of pep8

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ import json
 from models.engine.file_storage import FileStorage
 import models
 import unittest
+import pep8
 from models import storage
 from models.base_model import BaseModel
 from models.city import City
@@ -15,6 +16,17 @@ from models.review import Review
 from os import path
 import os
 from datetime import datetime
+
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 
 class fileestoraesoepapa(unittest.TestCase):
