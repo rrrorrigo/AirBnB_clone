94af5ee3031ef8ec3dcae81da51ddd9b85814fcf
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-29 23:05:17 -0300

    fixed pep8

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,7 @@ class BaseModel():
             for key, value, in kwargs.items():
                     if key != "__class__":
                         setattr(self, key, value)
-            if (hasattr(self, "created_at") and
-                type(self.created_at) is str):
+            if (hasattr(self, "created_at") and type(self.created_at) is str):
                     a = datetime.strptime(kwargs["created_at"], timeform)
                     self.created_at = a
             if (hasattr(self, "updated_at") and
