04680147d6e6beed77b26d68d689118aa88ed627
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-30 06:08:08 -0700

    testing robadinia

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -26,6 +26,59 @@ class UserTest(unittest.TestCase):
         """test instance"""
         user = User()
         self.assertIsInstance(user, BaseModel)
+        self.assertTrue(hasattr(user, "id"))
+        self.assertTrue(hasattr(user, "created_at"))
+        self.assertTrue(hasattr(user, "updated_at"))
+
+    def test_email_attr(self):
+        """ test if it generate unique id"""
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertEqual(user.email, "")
+
+    def test_password_attr(self):
+        """ test if it generate unique id"""
+        user = User()
+        self.assertTrue(hasattr(user, "password"))
+        self.assertEqual(user.password, "")
+
+    def test_first_name_attr(self):
+        """ test if it generate unique id"""
+        user = User()
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertEqual(user.first_name, "")
+
+    def test_last_name_attr(self):
+        """ test if it generate unique id"""
+        user = User()
+        self.assertTrue(hasattr(user, "last_name"))
+        self.assertEqual(user.last_name, "")
+
+    def test_to_dict_creates_dict(self):
+        """ test if it generate unique id"""
+        u = User()
+        new_d = u.to_dict()
+        self.assertEqual(type(new_d), dict)
+        for attr in u.__dict__:
+            self.assertTrue(attr in new_d)
+            self.assertTrue("__class__" in new_d)
+
+    def test_to_dict_values(self):
+        """ test if it generate unique id"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        u = User()
+        new_d = u.to_dict()
+        self.assertEqual(new_d["__class__"], "User")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], u.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], u.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """ test if it generate unique id"""
+        user = User()
+        string = "[User] ({}) {}".format(user.id, user.__dict__)
+        self.assertEqual(string, str(user))
 
     def test_unique_id(self):
         """ test if it generate unique id"""
