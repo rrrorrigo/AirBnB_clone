9da9648a54b51f399563cf17bcccdd8a11ac49f0
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-29 23:54:12 -0300

    base models indent fixed

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,14 @@ class BaseModel():
         """ Initialization of class base model"""
         if kwargs:
             for key, value, in kwargs.items():
-                    if key != "__class__":
-                        setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
             if (hasattr(self, "created_at") and type(self.created_at) is str):
-                    a = datetime.strptime(kwargs["created_at"], timeform)
-                    self.created_at = a
+                a = datetime.strptime(kwargs["created_at"], timeform)
+                self.created_at = a
             if (hasattr(self, "updated_at") and type(self.updated_at) is str):
-                    a = datetime.strptime(kwargs["updated_at"], timeform)
-                    self.updated_at = a
+                a = datetime.strptime(kwargs["updated_at"], timeform)
+                self.updated_at = a
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
