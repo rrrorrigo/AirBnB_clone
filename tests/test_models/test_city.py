ad56fe35ba3fe14b9bc12e74edc881a3f7e801fb
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-30 12:11:27 -0700

    chanign for city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -13,42 +13,28 @@ City = city.City
 
 
 class TestCityDocs(unittest.TestCase):
-    """Tests to check the documentation and style of City class"""
+    """test cases for class object Amenity"""
     @classmethod
     def setUpClass(cls):
-        """Set up for the doc tests"""
+        """test cases for class object Amenity"""
         cls.city_f = inspect.getmembers(City, inspect.isfunction)
 
-    def test_pep8_conformance_city(self):
-        """Test that models/city.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_city(self):
-        """Test that tests/test_models/test_city.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_city_module_docstring(self):
-        """Test for the city.py module docstring"""
+    def test_city_module(self):
+        """test cases for class object Amenity"""
         self.assertIsNot(city.__doc__, None,
                          "city.py needs a docstring")
         self.assertTrue(len(city.__doc__) >= 1,
                         "city.py needs a docstring")
 
-    def test_city_class_docstring(self):
-        """Test for the City class docstring"""
+    def test_city_class(self):
+        """test cases for class object Amenity"""
         self.assertIsNot(City.__doc__, None,
                          "City class needs a docstring")
         self.assertTrue(len(City.__doc__) >= 1,
                         "City class needs a docstring")
 
-    def test_city_func_docstrings(self):
-        """Test for the presence of docstrings in City methods"""
+    def test_city_func(self):
+        """test cases for class object Amenity"""
         for func in self.city_f:
             self.assertIsNot(func[1].__doc__, None,
                              "{:s} method needs a docstring".format(func[0]))
@@ -57,29 +43,29 @@ class TestCityDocs(unittest.TestCase):
 
 
 class TestCity(unittest.TestCase):
-    """Test the City class"""
+    """test cases for class object Amenity"""
     def test_is_subclass(self):
-        """Test that City is a subclass of BaseModel"""
+        """test cases for class object Amenity"""
         city = City()
         self.assertIsInstance(city, BaseModel)
         self.assertTrue(hasattr(city, "id"))
         self.assertTrue(hasattr(city, "created_at"))
         self.assertTrue(hasattr(city, "updated_at"))
 
-    def test_name_attr(self):
-        """Test that City has attribute name, and it's an empty string"""
+    def test_nam(self):
+        """test cases for class object Amenity"""
         city = City()
         self.assertTrue(hasattr(city, "name"))
         self.assertEqual(city.name, "")
 
-    def test_state_id_attr(self):
-        """Test that City has attribute state_id, and it's an empty string"""
+    def test_st(self):
+        """test cases for class object Amenity"""
         city = City()
         self.assertTrue(hasattr(city, "state_id"))
         self.assertEqual(city.state_id, "")
 
-    def test_to_dict_creates_dict(self):
-        """test to_dict method creates a dictionary with proper attrs"""
+    def test_to_dic(self):
+        """test cases for class object Amenity"""
         c = City()
         new_d = c.to_dict()
         self.assertEqual(type(new_d), dict)
@@ -87,8 +73,8 @@ class TestCity(unittest.TestCase):
             self.assertTrue(attr in new_d)
             self.assertTrue("__class__" in new_d)
 
-    def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
+    def test_to_dict(self):
+        """test cases for class object Amenity"""
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
         c = City()
         new_d = c.to_dict()
@@ -99,7 +85,7 @@ class TestCity(unittest.TestCase):
         self.assertEqual(new_d["updated_at"], c.updated_at.strftime(t_format))
 
     def test_str(self):
-        """test that the str method has the correct output"""
+        """test cases for class object Amenity"""
         city = City()
         string = "[City] ({}) {}".format(city.id, city.__dict__)
         self.assertEqual(string, str(city))
