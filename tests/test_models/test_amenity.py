0ed6455852cd9e7347473b1712e496643aa69505
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-30 11:55:38 -0700

    testing for javy

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,38 +11,9 @@ import pep8
 import inspect
 
 
-class TestAmenityDocs(unittest.TestCase):
-    """test cases for class object Amenity"""
-    @classmethod
-    def setUpClass(cls):
-        """test cases for class object Amenity"""
-        cls.amenity_f = inspect.getmembers(Amenity, inspect.isfunction)
-
-    def test_amenity_module_docstring(self):
-        """test cases for class object Amenity"""
-        self.assertIsNot(Amenity.__doc__, None,
-                         "amenity.py needs a docstring")
-        self.assertTrue(len(Amenity.__doc__) >= 1,
-                        "amenity.py needs a docstring")
-
-    def test_amenity_class_docstring(self):
-        """test cases for class object Amenity"""
-        self.assertIsNot(Amenity.__doc__, None,
-                         "Amenity class needs a docstring")
-        self.assertTrue(len(Amenity.__doc__) >= 1,
-                        "Amenity class needs a docstring")
-
-    def test_amenity_func_docstrings(self):
-        """test cases for class object Amenity"""
-        for func in self.amenity_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
-
-
 class AmenityTest(unittest.TestCase):
         """test cases for class object Amenity"""
+        @classmethod
         def test_Init(self):
                 """test instance"""
                 t = Amenity()
