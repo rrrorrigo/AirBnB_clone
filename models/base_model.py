aca1564b34c2e1d84ce9af437edd5ad2099463f8
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-29 23:02:51 -0300

    added docstring

--- models/base_model.py
+++ models/base_model.py
@@ -12,34 +12,38 @@ class BaseModel():
     """ Class BaseModel that defines all common
     attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-            if kwargs:
-                for key, value, in kwargs.items():
-                        if key != "__class__":
-                            setattr(self, key, value)
-                if (hasattr(self, "created_at") and
-                   type(self.created_at) is str):
-                        a = datetime.strptime(kwargs["created_at"], timeform)
-                        self.created_at = a
-                if (hasattr(self, "updated_at") and
-                   type(self.updated_at) is str):
-                        a = datetime.strptime(kwargs["updated_at"], timeform)
-                        self.updated_at = a
-            else:
-                self.id = str(uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = self.created_at
-                models.storage.new(self)
-                models.storage.save()
+        """ Initialization of class base model"""
+        if kwargs:
+            for key, value, in kwargs.items():
+                    if key != "__class__":
+                        setattr(self, key, value)
+            if (hasattr(self, "created_at") and
+                type(self.created_at) is str):
+                    a = datetime.strptime(kwargs["created_at"], timeform)
+                    self.created_at = a
+            if (hasattr(self, "updated_at") and
+               type(self.updated_at) is str):
+                    a = datetime.strptime(kwargs["updated_at"], timeform)
+                    self.updated_at = a
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
+        """ String representation of class BaseModel"""
         __name = type(self).__name__
         return "[{}] ({}) {}".format(__name, self.id, self.__dict__)
 
     def save(self):
+        """ Save the object into .json file"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """ return an dictionary representation of object"""
         rdict = dict(self.__dict__)
         rdict["__class__"] = type(self).__name__
         rdict["created_at"] = self.created_at.isoformat()
