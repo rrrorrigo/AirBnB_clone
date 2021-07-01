f4437bff4eb073c52fb1572437e5246a6fa19c61
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-30 22:19:50 -0300

    added pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,8 +9,25 @@ from io import StringIO
 
 
 class ConsoleTest(unittest.TestCase):
-        """ test cases for console"""
-        def test_helpcommand(self):
-                with patch('sys.stdout', new=StringIO()) as f:
-                        HBNBCommand().onecmd("show randomname")
-                        self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+    """ test cases for console"""
+    def test_helpcommand(self):
+        """ test cases for help command"""
+        m1 = "\nDocumented commands (type help <topic>):\n"
+        m2 = "========================================\n"
+        m3 = "EOF  all  count  create  destroy  help  quit  show  update\n\n"
+        hmsg = m1 + m2 + m3
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help randomname")
+            self.assertEqual(f.getvalue(), "*** No help on randomname\n")
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+            self.assertEqual(f.getvalue(), " exit of command interpreter\n")
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            self.assertEqual(f.getvalue(), " exit of command interpreter\n")
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+            self.assertEqual(f.getvalue(), hmsg)
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            self.assertEqual(f.getvalue(), " exit of command interpreter\n")
